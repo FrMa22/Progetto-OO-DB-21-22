ed820020995da717b58a705ae1016526e88ac675
FrMa22 <97689398+FrMa22@users.noreply.github.com>
2022-01-14 18:02:41 +0100

    All_SQLs.sql
    Codice modificato con maiuscole per i comandi, aggiunta di commenti ed eliminazione di NOT NULL superflui.

--- Basi di Dati/All_SQLs.sql
+++ Basi di Dati/All_SQLs.sql
@@ -1,90 +1,132 @@
-create table Rubrica(
-Utente_ID varchar(30) NOT NULL,
+--Crea la tabella Rubrica
+CREATE TABLE Rubrica(
+Utente_ID VARCHAR(30),
+--Chiave primaria
 PRIMARY KEY(Utente_ID) );
 
-create table Gruppo(
-Gruppo_ID serial ,
-Nome varchar(20) NOT NULL,
-Rubrica_FK varchar(30) NOT NULL,
+--Crea la tabella Gruppo
+CREATE TABLE Gruppo(
+ --Gruppo_ID è di tipo SERIAL
+Gruppo_ID SERIAL ,
+Nome VARCHAR(20) NOT NULL,
+Rubrica_FK VARCHAR(30) NOT NULL,
+ --Chiave primaria
 PRIMARY KEY(Gruppo_ID),
-FOREIGN KEY(Rubrica_FK) references Rubrica(Utente_ID)
+ --Chiave esterna
+FOREIGN KEY(Rubrica_FK) REFERENCES Rubrica(Utente_ID)
  );
 
-create table Contatto(
-Contatto_ID serial NOT NULL,
-Nome varchar(20) NOT NULL,
-SecondoNome varchar(20),
-Cognome varchar(20) NOT NULL,
-Foto varchar(20),
-Rubrica_FK varchar(20) NOT NULL,
+--Crea la tabella Contatto
+CREATE TABLE Contatto(
+ --Contatto_ID è di tipo SERIAL
+Contatto_ID SERIAL ,
+Nome VARCHAR(20) NOT NULL,
+SecondoNome VARCHAR(20),
+Cognome VARCHAR(20) NOT NULL,
+Foto VARCHAR(20),
+Rubrica_FK VARCHAR(20) NOT NULL,
+ --Chiave primaria
 PRIMARY KEY(Contatto_ID),
-FOREIGN KEY(Rubrica_FK) references Rubrica(Utente_ID)
+ --Chiave esterna
+FOREIGN KEY(Rubrica_FK) REFERENCES Rubrica(Utente_ID)
 );
 
-create table Account(
-Account_ID serial NOT NULL,
-Fornitore varchar(20) NOT NULL,
-IndirizzoEmail varchar(256) NOT NULL UNIQUE CHECK ( IndirizzoEmail LIKE '_%@_%.__%'),
-FraseStato varchar(256),
-Nickname varchar(30),
+--Crea la tabella Account
+CREATE TABLE Account(
+ --Account_ID è di tipo SERIAL
+Account_ID SERIAL ,
+Fornitore VARCHAR(20) NOT NULL,
+ --L'indirizzo email deve essere strutturato in quest'ordine: testo,"@",testo,".",testo utilizzando un CHECK per verificare tale condizione
+IndirizzoEmail VARCHAR(256) NOT NULL UNIQUE CHECK ( IndirizzoEmail LIKE '_%@_%.__%'),
+FraseStato VARCHAR(256),
+Nickname VARCHAR(30),
+ --Chiave primaria
 PRIMARY KEY(Account_ID)
-); 
+);
 
-create table Email(
-Email_ID serial NOT NULL,
-IndirizzoEmail varchar(256) NOT NULL UNIQUE CHECK (IndirizzoEmail LIKE '_%@_%.__%'),
-Descrizione varchar(30),
-Contatto_ID serial NOT NULL,
+--Crea la tabella Email
+CREATE TABLE Email(
+ --Email_ID è di tipo SERIAL
+Email_ID SERIAL ,
+ --L'indirizzo email deve essere strutturato in quest'ordine: testo,"@",testo,".",testo utilizzando un CHECK per verificare tale condizione
+IndirizzoEmail VARCHAR(256) NOT NULL UNIQUE CHECK (IndirizzoEmail LIKE '_%@_%.__%'),
+Descrizione VARCHAR(30),
+Contatto_ID SERIAL ,
+ --Chiave primaria
 PRIMARY KEY(Email_ID),
-FOREIGN KEY(Contatto_ID) references Contatto(Contatto_ID)
+ --Chiave esterna
+FOREIGN KEY(Contatto_ID) REFERENCES Contatto(Contatto_ID)
 );
 
-create table Indirizzo(
-Indirizzo_ID serial NOT NULL,
-Via varchar(20) NOT NULL,
-Città varchar(30),
-Nazione varchar(30) NOT NULL,
-CAP integer NOT NULL,
-Descrizione varchar(20) NOT NULL,
-Contatto_ID serial NOT NULL,
+--Crea la tabella Indirizzo
+CREATE TABLE Indirizzo(
+ --Indirizzo_ID è di tipo SERIAL
+Indirizzo_ID SERIAL ,
+Via VARCHAR(20) NOT NULL,
+Città VARCHAR(30),
+Nazione VARCHAR(30) NOT NULL,
+CAP INTEGER NOT NULL,
+Descrizione VARCHAR(20) NOT NULL,
+Contatto_ID SERIAL ,
+ --Chiave primaria
 PRIMARY KEY(Indirizzo_ID),
-FOREIGN KEY(Contatto_ID) references Contatto(Contatto_ID)
+ --Chiave esterna
+FOREIGN KEY(Contatto_ID) REFERENCES Contatto(Contatto_ID)
 );
 
-create table Telefono (
-Telefono_ID serial NOT NULL,
-Numero integer NOT NULL,
-Descrizione varchar(20),
-Contatto_ID serial NOT NULL,
+--Crea la tabella Telefono
+CREATE TABLE Telefono (
+ --Telefono_ID è di tipo SERIAL
+Telefono_ID SERIAL,
+Numero INTEGER NOT NULL,
+Descrizione VARCHAR(20),
+Contatto_ID SERIAL ,
+ --Chiave primaria
 PRIMARY KEY(Telefono_ID),
-FOREIGN KEY(Contatto_ID) references Contatto(Contatto_ID) );
+--Chiave esterna
+FOREIGN KEY(Contatto_ID) REFERENCES Contatto(Contatto_ID) );
 
-create table Associa (
-Contatto_ID serial NOT NULL,
-Account_ID serial NOT NULL,
-FOREIGN KEY(Contatto_ID) references Contatto(Contatto_ID),
-FOREIGN KEY(Account_ID) references Account(Account_ID) );
+--Crea la tabella Associa 
+CREATE TABLE Associa (
+--Contatto_ID e Account_ID sono di tipo SERIAL
+Contatto_ID SERIAL ,
+Account_ID SERIAL,
+--Chiavi esterne
+FOREIGN KEY(Contatto_ID) REFERENCES Contatto(Contatto_ID),
+FOREIGN KEY(Account_ID) REFERENCES Account(Account_ID) );
 
-create table RecapitoEmail(
-Contatto_ID serial NOT NULL,
-Email_ID serial NOT NULL,
-FOREIGN KEY(Contatto_ID) references Contatto(Contatto_ID),
-FOREIGN KEY(Email_ID) references Email(Email_ID) );
+--Crea la tabella RecapitoEmail
+CREATE TABLE RecapitoEmail(
+ --Contatto_ID e Email_ID sono di tipo SERIAL
+Contatto_ID SERIAL ,
+Email_ID SERIAL ,
+--Chiavi esterne
+FOREIGN KEY(Contatto_ID) REFERENCES Contatto(Contatto_ID),
+FOREIGN KEY(Email_ID) REFERENCES Email(Email_ID) );
 
-create table RecapitoIndirizzo(
-Contatto_ID serial NOT NULL,
-Indirizzo_ID serial NOT NULL,
-FOREIGN KEY(Contatto_ID) references Contatto(Contatto_ID),
-FOREIGN KEY(Indirizzo_ID) references Indirizzo(Indirizzo_ID) );
+--Crea la tabella RecapitoIndirizzo
+CREATE TABLE RecapitoIndirizzo(
+ --Contatto_ID e Indirizzo_ID sono di tipo SERIAL
+Contatto_ID SERIAL ,
+Indirizzo_ID SERIAL ,
+ --Chiavi esterne
+FOREIGN KEY(Contatto_ID) REFERENCES Contatto(Contatto_ID),
+FOREIGN KEY(Indirizzo_ID) REFERENCES Indirizzo(Indirizzo_ID) );
 
-create table RecapitoTelefono(
-Contatto_ID serial NOT NULL,
-Telefono_ID serial NOT NULL,
-FOREIGN KEY(Contatto_ID) references Contatto(Contatto_ID),
-FOREIGN KEY(Telefono_ID) references Telefono(Telefono_ID) );
+--Crea la tabella RecapitoTelefono
+CREATE TABLE RecapitoTelefono(
+ --Contatto_ID e Telefono_ID sono di tipo SERIAL
+Contatto_ID SERIAL,
+Telefono_ID SERIAL ,
+ --Chiavi esterne
+FOREIGN KEY(Contatto_ID) REFERENCES Contatto(Contatto_ID),
+FOREIGN KEY(Telefono_ID) REFERENCES Telefono(Telefono_ID) );
 
-create table Composizione(
-Contatto_ID serial NOT NULL,
-Gruppo_ID serial  NOT NULL,
-FOREIGN KEY(Contatto_ID) references Contatto(Contatto_ID),
-FOREIGN KEY(Gruppo_ID) references Gruppo(Gruppo_ID) );
+--Crea la tabella Composizione
+CREATE TABLE Composizione(
+ --Contatto_ID e Gruppo_ID sono di tipo SERIAL
+Contatto_ID SERIAL,
+Gruppo_ID SERIAL  ,
+ --Chiavi esterne
+FOREIGN KEY(Contatto_ID) REFERENCES Contatto(Contatto_ID),
+FOREIGN KEY(Gruppo_ID) REFERENCES Gruppo(Gruppo_ID) );
